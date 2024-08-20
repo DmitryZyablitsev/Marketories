9239330e71707059073c22f13163eb571db143ed
dima <dima@dmitryz.netis.cc>
2024-08-20 14:31:30 +0500

    fix-nav-panel-and-spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,7 @@ FactoryBot.define do
   factory :user do
     email
     password { '12345678' }
+    password_confirmation { '12345678' }
+    confirmed_at { '2024-06-06 22:30:45.121653778 +0500' }
   end
 end
