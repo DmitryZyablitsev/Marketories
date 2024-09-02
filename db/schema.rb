933f2c31d37af2263c4031ac48fb3f8bd3baf742
dima <dima@dmitryz.netis.cc>
2024-09-02 15:28:54 +0500

    migrate-add-role-to-users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_15_052423) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_27_124934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +37,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_15_052423) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "role", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 
   add_foreign_key "products", "users"
